874a46da99eaf86c6a3edfb93437e7257fdae54b
Caspar Florian Ebeling <florian.ebeling@gmail.com>
2009-06-02 22:45:31 +0200

    newly generated spec scripts

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,17 @@
 # This file is copied to ~/spec when you run 'ruby script/generate rspec'
 # from the project root directory.
-ENV["RAILS_ENV"] = "test"
-require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
-require 'spec'
+ENV["RAILS_ENV"] ||= 'test'
+require File.dirname(__FILE__) + "/../config/environment" unless defined?(RAILS_ROOT)
+require 'spec/autorun'
 require 'spec/rails'
 
 Spec::Runner.configure do |config|
   # If you're not using ActiveRecord you should remove these
   # lines, delete config/database.yml and disable :active_record
   # in your config/boot.rb
-  #config.use_transactional_fixtures = true
-  #config.use_instantiated_fixtures  = false
-  #config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
+#   config.use_transactional_fixtures = true
+#   config.use_instantiated_fixtures  = false
+#   config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
 
   # == Fixtures
   #
@@ -39,9 +39,9 @@ Spec::Runner.configure do |config|
   #
   # config.mock_with :mocha
   # config.mock_with :flexmock
-  #config.mock_with :rr
+  # config.mock_with :rr
   #
   # == Notes
   # 
-  # For more information take a look at Spec::Example::Configuration and Spec::Runner
+  # For more information take a look at Spec::Runner::Configuration and Spec::Runner
 end
