a8d24662879f8bfeac1587153172ae7ef13f552a
Caspar Florian Ebeling <florian.ebeling@gmail.com>
2009-06-06 17:08:09 +0200

    add auto-discovery link for rss

--- spec/views/builds/index.html.erb_spec.rb
+++ spec/views/builds/index.html.erb_spec.rb
@@ -4,10 +4,14 @@ describe "/builds/index" do
   before(:each) do
     builds = mock("paged_array", :total_pages => 1, :each => nil)
     assigns[:builds] = builds
-    render 'builds/index'
+    render 'builds/index', :layout => 'application'
   end
   
   it "should tell you where to find the file" do
     response.should have_tag('div', /Timeline/)
+  end
+
+  it "should have a feed autodiscovery link" do
+    response.should have_tag(%q{link[title=RSS][rel=alternate][type=application/rss+xml]})
   end
 end
